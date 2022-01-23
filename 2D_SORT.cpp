5a366fd445cdcc77b285212c3d1691351dce8a33
MinaMalak-cmd <engmina15@yahoo.com>
2022-01-23 13:57:38 +0200

    first trial 2D_SORT.cpp

---
+++ 2D_SORT.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+//https://vjudge.net/contest/476841#problem/A
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t,n;
+    cin >>
+
+    return 0;
+}
