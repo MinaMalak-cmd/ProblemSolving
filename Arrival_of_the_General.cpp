42a059638699d15831d556143fb8f8f2a492c644
Mina Malak <mina.malak@tactful.ai>
2022-06-23 11:44:26 +0200

    finished arrival_of_the_general

--- Arrival_of_the_General.cpp
+++ Arrival_of_the_General.cpp
@@ -1,4 +1,5 @@
 //https://vjudge.net/contest/500670#problem/B
+//what's missing look for better solution
 
 #include <iostream>
 #include<algorithm>
@@ -7,7 +8,7 @@ using namespace std;
 int main()
 {
     int n; cin >>n;
-    int heights[n],seconds=0;
+    int heights[n],seconds=0,minIndex=0;
     for(int i=0;i<n;i++){
       cin >>heights[i];
     }
@@ -16,9 +17,19 @@ int main()
     sort(arr,arr+n);
     int max=arr[n-1];
     int min = arr[0];
-    for(int i=1,j=n-1;i<n;i++,j--){
+    for(int j=n-1;j>=0;j--){
         if(heights[j]==min){
+            minIndex = j;
             seconds +=(n-1)-j;
+            break;
+        }
+    }
+    for(int i=0;i<n;i++){
+        if(heights[i]==max){
+            seconds +=i;
+            if(i>minIndex) {
+                seconds -=1;
+            }
             break;
         }
     }
