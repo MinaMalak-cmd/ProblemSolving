22a49fa097c9ec6f7ebd61a0d24a96d334376896
Mina Malak <mina.malak@tactful.ai>
2022-06-26 21:47:55 +0200

    solve vasya_and_the_hipster problem

--- How_To_know_if_a_word_is_homogenuous.cpp
+++ How_To_know_if_a_word_is_homogenuous.cpp
@@ -24,3 +24,37 @@ int main()
     cout << flag;
     return 0;
 }
+
+//Soln in general
+/*
+#include <iostream>
+#include <stack> 
+
+using namespace std;
+
+int main()
+{
+    string s; cin >>s;
+    stack<char> stack;
+    for(int i=0;i<s.size();i++){
+        if(s[i]=='(' || s[i]=='[' || s[i]=='{'){
+            stack.push(s[i]);
+        }
+        else if((s[i]==')' && stack.top()=='(') || (s[i]==']' && stack.top()=='[') || (s[i]=='}' && stack.top()=='{')){
+            stack.pop();
+        }
+        else{
+            cout << "No";
+            return 0;
+        }
+    }
+    string flag=!stack.empty()? "No":"yes";
+    cout << flag;
+    return 0;
+}
+
+
+
+
+
+*/
