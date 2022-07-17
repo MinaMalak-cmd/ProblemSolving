8457783ae3c3b188e0e5707363cbf13e1aefd231
Mina Malak <mina.malak@tactful.ai>
2022-07-17 18:13:20 +0200

    forming a triangle

---
+++ Forming_a_triangle.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <bits/stdc++.h> 
+using namespace std;
+
+int main()
+{
+    int n; cin >>n;
+    int arr[n];
+    
+    for(int i=0;i<n;i++){
+        cin >>arr[i];
+    }
+    
+    sort(arr,arr+n);
+    for(int i=0;i<n-2;i++){
+        if(arr[i]+arr[i+1]>arr[i+2]){
+            cout << "YES";
+            return 0;
+        }
+    }
+    cout << "NO";
+    return 0;
+}
